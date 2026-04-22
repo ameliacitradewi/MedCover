941f689befba8783c384cab8c21ab5c06e373a10
Amelia Citra <ameliacitradewi@gmail.com>
2026-04-22 13:07:15 +0700

    Initial Commit

---
+++ MedCover/MedCoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedCoverApp.swift
+//  MedCover
+//
+//  Created by Amelia Citra on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MedCoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
